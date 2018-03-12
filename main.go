ef737963bd7d2951374f0339d9fd3200a4ff24d7
David Ko <david.ko@velvetreactor.com>
2018-03-12 07:48:45 -0400

    Add sessions

--- main.go
+++ main.go
@@ -2,24 +2,40 @@ package main
 
 import (
 	"fmt"
-	"reflect"
+	"net/http"
 
-	"gopkg.in/labstack/echo.v3"
+	"github.com/gorilla/sessions"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo-contrib/session"
 )
 
-type MyType struct {
-	Id   string
-	Name string
-}
+// type MyType struct {
+// }
 
-func (mt *MyType) Foob() {
-	fmt.Println("Called foob")
-}
+// func main() {
+// 	e := echo.New()
+// 	e.Group("/admin")
+// 	mt := MyType{}
+// 	vof := reflect.ValueOf(mt)
+// 	fmt.Println(vof.Type())
+// }
 
 func main() {
 	e := echo.New()
-	e.Group("/admin")
-	mt := &MyType{}
-	vof := reflect.ValueOf(mt)
-	vof.MethodByName("Foob").Call([]reflect.Value{})
+	e.Use(session.Middleware(sessions.NewCookieStore([]byte("secret"))))
+	e.GET("/", func(ctx echo.Context) error {
+		sess, _ := session.Get("session", ctx)
+		sess.Options = &sessions.Options{
+			Path:     "/",
+			MaxAge:   86400,
+			HttpOnly: true,
+		}
+		// sess.Values["foo"] = "baz"
+		// sess.Values["a"] = 101
+		sess.Save(ctx.Request(), ctx.Response())
+		fmt.Println(sess)
+		return ctx.NoContent(http.StatusOK)
+	})
+
+	e.Logger.Fatal(e.Start(":3000"))
 }
