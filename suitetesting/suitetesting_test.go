56e5c6eefc66c7f8dad9ac1f4e68538fa6bb101f
David Ko <david.ko@pvtmethod.com>
2018-06-20 14:17:58 -0400

    Tests

--- suitetesting/suitetesting_test.go
+++ suitetesting/suitetesting_test.go
@@ -1,1 +1,19 @@
 package suitetesting
+
+import (
+	"testing"
+)
+
+func Suite(t *testing.T, impl Thinger) {
+	impl.DoThing()
+}
+
+func TestThinger_thingerOne(t *testing.T) {
+	o := &OneThinger{}
+	Suite(t, o)
+}
+
+func TestThinger_thingerTwo(t *testing.T) {
+	o := &TwoThinger{}
+	Suite(t, o)
+}
