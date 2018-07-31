f5f781ca218c0d8caee42a6001c952a8c7ec967a
David Ko <david.ko@damncarousel.com>
2018-07-31 07:51:54 -0400

    Partial implementation of links package

---
+++ links/links.go
@@ -0,0 +1,49 @@
+package links
+
+import (
+	"net/http"
+)
+
+// Extract:
+// 1. Makes HTTP get
+// 2. Parses response
+// 3. Returns all links in document
+
+func Extract(url string) ([]string, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		resp.Body.Close() // [ANKI] always close the resp.Body
+		return nil, fmt.Errorf("getting %s: %s", url, resp.Status)
+	}
+
+	doc, err := html.Parse(resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		return nil, fmt.Errorf("parsing %s as HTML: %v", url, err)
+	}
+
+	var links []string
+	visitNode := func(n *html.Node) {
+		if n.Type == html.ElementNode && n.Data == "a" {
+			for _, a := range n.Attr {
+				if a.Key != "href" {
+					continue // skip if HTML element is NOT a link
+				}
+				link, err := resp.Request.URL.Parse(a.Val)
+				if err != nil {
+					continue
+				}
+				links = append(links, link.String())
+			}
+		}
+	}
+	forEachNode(doc, visitNode, nil)
+}
+
+func forEachNode(n *html.Node, pre, post func(n *html.Node)) {
+
+}
