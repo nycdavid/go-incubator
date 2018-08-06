639996937e9278e8452c818d89ef7aae0eb39f65
David Ko <david.ko@damncarousel.com>
2018-08-06 06:32:07 -0400

    Retab

--- findlinks/main.go
+++ findlinks/main.go
@@ -8,21 +8,40 @@ import (
 )
 
 func main() {
-	breadthFirst(crawl, []string{"https://www.google.com"})
+	firstUrl := []string{"https://blog.golang.org"}
+	worklist := make(chan []string)
+	go func() { worklist <- firstUrl }()
+
+	seen := make(map[string]bool)
+
+	for list := range worklist {
+		for _, link := range list {
+			if !seen[link] {
+				seen[link] = true
+				go func(link string) {
+					worklist <- crawl(link)
+				}(link)
+			}
+		}
+	}
 }
 
+var tokens = make(chan struct{}, 10)
+
 func crawl(url string) []string {
-	fmt.Println(url)
+	// fmt.Println(url)
+	tokens <- struct{}{}
 	links, err := links.Extract(url)
+	<-tokens
 	if err != nil {
-		log.Fatal(err)
+		log.Print(err)
 	}
 	return links
 }
 
 // Two arguments:
 // 1. f: A function that receives a string as an argument and
-// 		returns a slice of strings
+//		returns a slice of strings
 // 2. worklist: a slice of strings
 func breadthFirst(crawlFunc func(item string) []string, worklist []string) {
 	seen := make(map[string]bool)
